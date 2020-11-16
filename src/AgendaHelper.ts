70a9828e5ba552658c251636d101a3e37b8e5e12
zawette <zawette@gmail.com>
2020-11-16 13:42:29 +0100

    f

--- src/AgendaHelper.ts
+++ src/AgendaHelper.ts
@@ -1,17 +1,17 @@
-export const daysOftheWeek = ["Mo", "Tu", "We", "Th", "Fr", "Sa", "Su"];
+export const daysOftheWeek = ['Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa', 'Su'];
 export const months = [
-  { full: "January", short: "Jan" },
-  { full: "February", short: "Feb" },
-  { full: "March", short: "Mar" },
-  { full: "April", short: "Apr" },
-  { full: "May", short: "May" },
-  { full: "June", short: "June" },
-  { full: "July", short: "July" },
-  { full: "August", short: "Aug" },
-  { full: "September", short: "Sept" },
-  { full: "October", short: "Oct" },
-  { full: "November", short: "Nov" },
-  { full: "December", short: "Dec" }
+  { full: 'January', short: 'Jan' },
+  { full: 'February', short: 'Feb' },
+  { full: 'March', short: 'Mar' },
+  { full: 'April', short: 'Apr' },
+  { full: 'May', short: 'May' },
+  { full: 'June', short: 'June' },
+  { full: 'July', short: 'July' },
+  { full: 'August', short: 'Aug' },
+  { full: 'September', short: 'Sept' },
+  { full: 'October', short: 'Oct' },
+  { full: 'November', short: 'Nov' },
+  { full: 'December', short: 'Dec' },
 ];
 
 export let shiftArray = (arr: Array<any>, shiftBy: number) => {
